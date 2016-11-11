f0b88f278c539293a33f9df90d253a11f23c8e31
Ernesto Cortez <Ernesto Cortez>
2016-11-10 23:18:25 -0800

    Implment isWord() and add() function in TrieNoe

--- app/src/main/java/com/google/engedu/ghost/TrieNode.java
+++ app/src/main/java/com/google/engedu/ghost/TrieNode.java
@@ -1,5 +1,7 @@
 package com.google.engedu.ghost;
 
+import org.w3c.dom.Node;
+
 import java.util.HashMap;
 
 
@@ -13,14 +15,53 @@ public class TrieNode {
     }
 
     public void add(String s) {
+        add(s, 0);
+    }
+
+    private void add(String s, int index){
+        if(index == s.length()){
+            isWord = true;
+            return;
+        }
+        String current = s.substring(index, 1);
+        TrieNode child = children.get(current);
+        if(child == null){
+            child = new TrieNode();
+            children.put(current, child);
+        }
+        child.add(s, index+1);
     }
 
     public boolean isWord(String s) {
-      return false;
+
+        return isWord(s, 0);
+    }
+
+    private boolean isWord(String s, int index){
+        if(index == s.length()){
+            return isWord;
+        }
+        String current = s.substring(index, 1);
+        TrieNode child = children.get(current);
+        return child.isWord(s, index+1);
     }
 
     public String getAnyWordStartingWith(String s) {
-        return null;
+
+        return getAnyWordStartingWith(s, 0);
+    }
+
+    private String getAnyWordStartingWith(String s, int index){
+        if(index == s.length()){
+            return getRandomWord()
+        }
+        String current = s.substring(index, 1);
+        TrieNode child = children.get(current);
+        return child.getAnyWordStartingWith(s, index+1);
+    }
+
+    private String getRandomWord(){
+
     }
 
     public String getGoodWordStartingWith(String s) {
